7913f0f9405faec51a56bf96f02a163f1d2bab76
alba-rg-14 <albaruizgutierrez@uma.es>
2023-02-15 20:19:45 +0100

    Test create person without name

--- src/test/java/org/example/PersonTest.java
+++ src/test/java/org/example/PersonTest.java
@@ -1,4 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.InvalidPropertiesFormatException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PersonTest {
-  
+
+    @Test
+    void CreatePersonWithoutName(){
+        assertThrows(InvalidPropertiesFormatException.class, () -> new Person(null,0,"Male"));
+    }
 }
