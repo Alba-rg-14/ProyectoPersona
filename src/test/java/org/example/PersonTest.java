4eaad3ae63e2e0cb02b7380665f4476564d879cf
alba-rg-14 <albaruizgutierrez@uma.es>
2023-02-15 20:20:43 +0100

    Test create person with wrong age

--- src/test/java/org/example/PersonTest.java
+++ src/test/java/org/example/PersonTest.java
@@ -13,4 +13,9 @@ class PersonTest {
     void CreatePersonWithoutName(){
         assertThrows(InvalidPropertiesFormatException.class, () -> new Person(null,0,"Male"));
     }
+
+    @Test
+    void CreatePersonWithWrongAge(){
+        assertThrows(InvalidPropertiesFormatException.class, () -> new Person("Alba",-5,"Female"));
+    }
 }
